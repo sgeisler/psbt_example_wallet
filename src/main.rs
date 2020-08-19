272252f6abd346118e51ab4cb849f00dcb536388
Sebastian Geisler <sebastian@blockstream.io>
2020-08-19 15:27:12 +0200

    impl basic w2pkh psbt pipeline

---
+++ src/main.rs
@@ -0,0 +1,399 @@
+use crate::responsibilities::{PsbtCreationError, PsbtWallet, SignPsbt};
+use bip39::Mnemonic;
+use bitcoin::blockdata::constants::max_target;
+use bitcoin::consensus::Encodable;
+use bitcoin::secp256k1::Secp256k1;
+use bitcoin::util::bip32::{
+    ChildNumber, DerivationPath, DerivationPathIterator, ExtendedPrivKey, ExtendedPubKey,
+    Fingerprint,
+};
+use bitcoin::util::misc::script_find_and_remove;
+use bitcoin::util::psbt::{Input, PartiallySignedTransaction};
+use bitcoin::{
+    Address, Amount, Network, OutPoint, PublicKey, Script, SigHashType, Transaction, TxIn, TxOut,
+    Txid,
+};
+use bitcoincore_rpc::bitcoincore_rpc_json::{ScanTxoutRequest, ScanUtxoResult, Utxo};
+use bitcoincore_rpc::{Auth, Client, Error, RpcApi};
+use itertools::zip;
+use miniscript::descriptor::DescriptorPublicKey;
+use miniscript::Descriptor;
+use secp256k1::{Message, Signature};
+use std::collections::{BTreeMap, HashMap};
+use std::fmt::Debug;
+use std::net::TcpStream;
+use std::str::FromStr;
+use structopt::StructOpt;
+
+mod responsibilities;
+
+const DUST_LIMIT: u64 = 546;
+
+pub struct NaiveWallet {
+    descriptor: Descriptor<DescriptorPublicKey>,
+    utxos: Vec<(DerivationPath, Utxo)>,
+    network: Network,
+    rpc: bitcoincore_rpc::Client,
+    feerate_sat_byte: f64,
+    gap_limit: u64,
+    internal_next_key: ChildNumber,
+    external_next_key: ChildNumber,
+}
+
+impl NaiveWallet {
+    pub fn new(
+        descriptor: Descriptor<DescriptorPublicKey>,
+        network: Network,
+        rpc: bitcoincore_rpc::Client,
+        gap_limit: u64,
+    ) -> Result<NaiveWallet, bitcoincore_rpc::Error> {
+        let internal = DerivationPath::from(&[ChildNumber::from_normal_idx(1).unwrap()][..]);
+        let mut internal_it = internal.normal_children();
+        let external = DerivationPath::from(&[ChildNumber::from_normal_idx(0).unwrap()][..]);
+        let mut external_it = external.normal_children();
+
+        let utxos: Vec<(DerivationPath, Utxo)> =
+            Self::fetch_utxos(&rpc, &descriptor, &mut internal_it, gap_limit)
+                .chain(Self::fetch_utxos(
+                    &rpc,
+                    &descriptor,
+                    &mut external_it,
+                    gap_limit,
+                ))
+                .collect::<Result<_, _>>()?;
+
+        fn child_num_sub(child: ChildNumber, sub: u32) -> ChildNumber {
+            match child {
+                ChildNumber::Normal { index } => ChildNumber::Normal { index: index - sub },
+                ChildNumber::Hardened { index } => ChildNumber::Hardened { index: index - sub },
+            }
+        }
+
+        Ok(NaiveWallet {
+            descriptor,
+            utxos,
+            network,
+            rpc,
+            feerate_sat_byte: 1.0,
+            gap_limit,
+            internal_next_key: child_num_sub(internal_it.next().unwrap()[..][1], gap_limit as u32),
+            external_next_key: child_num_sub(external_it.next().unwrap()[..][1], gap_limit as u32),
+        })
+    }
+
+    fn fetch_utxos<'a, 'b: 'a>(
+        rpc: &'a Client,
+        descriptor: &'a Descriptor<DescriptorPublicKey>,
+        children: &'a mut DerivationPathIterator<'b>,
+        gap_limit: u64,
+    ) -> FetchUtxoIterator<'a, 'b> {
+        FetchUtxoIterator {
+            rpc,
+            descriptor,
+            children,
+            gap_limit,
+            last_res: (DerivationPath::from(&[][..]), vec![]),
+            net: Network::Regtest,
+        }
+    }
+
+    fn descriptor_utxo_to_input(
+        utxo: Utxo,
+        desc: Descriptor<DescriptorPublicKey>,
+        path: DerivationPath,
+    ) -> Input {
+        let ctx = Secp256k1::new();
+        let mut hd_keypaths = BTreeMap::default();
+        desc.translate_pk(
+            |pk| -> Result<_, ()> {
+                match pk {
+                    DescriptorPublicKey::XPub(xpub) => {
+                        if let Some((fp, base_path)) = xpub.source.clone() {
+                            // TODO: impl DerivationPath concat
+                            let mut path: Vec<ChildNumber> = base_path.into();
+                            path.extend_from_slice(&xpub.derivation_path[..]);
+                            // TODO: impl path ffunction for DescriptorXKey
+                            // TODO: impl get real key
+                            hd_keypaths.insert(
+                                xpub.xkey
+                                    .derive_pub(&ctx, &xpub.derivation_path)
+                                    .unwrap()
+                                    .public_key,
+                                (fp, path.into()),
+                            );
+                        } else {
+                            hd_keypaths.insert(
+                                xpub.xkey
+                                    .derive_pub(&ctx, &xpub.derivation_path)
+                                    .unwrap()
+                                    .public_key,
+                                (xpub.xkey.fingerprint(), xpub.derivation_path.clone()),
+                            );
+                        }
+                    }
+                    DescriptorPublicKey::PubKey(_) => {}
+                }
+                Ok(String::new())
+            },
+            |_| Ok(String::new()),
+        );
+
+        let mut input = Input {
+            non_witness_utxo: None,
+            witness_utxo: None,
+            partial_sigs: Default::default(),
+            sighash_type: Some(SigHashType::All),
+            redeem_script: None,
+            witness_script: None,
+            hd_keypaths,
+            final_script_sig: None,
+            final_script_witness: None,
+            unknown: Default::default(),
+        };
+
+        match desc {
+            Descriptor::Bare(_) => unimplemented!(),
+            Descriptor::Pk(_) => unimplemented!(),
+            Descriptor::Pkh(_) => unimplemented!(),
+            Descriptor::Wpkh(_) => {
+                input.witness_utxo = Some(TxOut {
+                    value: utxo.amount.as_sat(),
+                    script_pubkey: utxo.script_pub_key,
+                });
+            }
+            Descriptor::ShWpkh(_) => unimplemented!(),
+            Descriptor::Sh(_) => unimplemented!(),
+            Descriptor::Wsh(_) => unimplemented!(),
+            Descriptor::ShWsh(_) => unimplemented!(),
+        }
+
+        input
+    }
+
+    pub fn total_funds(&self) -> Amount {
+        let funds: u64 = self
+            .utxos
+            .iter()
+            .map(|(_, utxo)| utxo.amount.as_sat())
+            .sum();
+        Amount::from_sat(funds)
+    }
+
+    pub fn new_addr(&mut self) -> Address {
+        let descr = self
+            .descriptor
+            .derive(&[ChildNumber::Normal { index: 0 }, self.external_next_key][..]);
+        self.external_next_key = self.external_next_key.increment().unwrap();
+
+        descr.address(self.network).unwrap()
+    }
+
+    fn new_change_addr(&mut self) -> Address {
+        let descr = self
+            .descriptor
+            .derive(&[ChildNumber::Normal { index: 1 }, self.internal_next_key][..]);
+        self.internal_next_key = self.internal_next_key.increment().unwrap();
+
+        descr.address(self.network).unwrap()
+    }
+}
+
+pub struct FetchUtxoIterator<'a, 'b: 'a> {
+    rpc: &'a Client,
+    descriptor: &'a Descriptor<DescriptorPublicKey>,
+    children: &'a mut DerivationPathIterator<'b>,
+    gap_limit: u64,
+    last_res: (DerivationPath, Vec<Utxo>),
+    net: Network,
+}
+
+impl<'a, 'b: 'a> Iterator for FetchUtxoIterator<'a, 'b> {
+    type Item = bitcoincore_rpc::Result<(DerivationPath, Utxo)>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut gap = 0;
+        while self.last_res.1.is_empty() {
+            if gap >= self.gap_limit {
+                return None;
+            }
+
+            let path = self
+                .children
+                .next()
+                .expect("infinite iterator for all intents and purposes");
+            let addr = self.descriptor.derive(&path[..]).address(self.net).unwrap();
+
+            let utxos = match self
+                .rpc
+                .scan_txout_set(&[ScanTxoutRequest::Single(format!("addr({})", addr))])
+            {
+                Ok(utxos) => utxos,
+                Err(e) => return Some(Err(e)),
+            }; //TODO: fix!!! maybe raw?!
+            self.last_res = (path, utxos.unspents);
+            gap += 1;
+        }
+
+        return Some(Ok((
+            self.last_res.0.clone(),
+            self.last_res
+                .1
+                .pop()
+                .expect("can't be empty, see while loop"),
+        )));
+    }
+}
+
+impl PsbtWallet for NaiveWallet {
+    type Error = &'static str;
+
+    fn create_transaction(
+        &mut self,
+        mut outputs: &[TxOut],
+    ) -> Result<PartiallySignedTransaction, PsbtCreationError<Self::Error>> {
+        // TODO: discuss making the argument a vec
+        let mut outputs = outputs.to_vec();
+
+        // TODO: calculate size, increase when adding inputs
+        let size_est_bytes = 400;
+        let min_amt_sat = outputs.iter().map(|out| out.value).sum::<u64>()
+            + ((self.feerate_sat_byte * size_est_bytes as f64) as u64);
+
+        let mut selected_utxos = vec![];
+        let mut in_sats = 0;
+        while let Some(utxo) = self.utxos.pop() {
+            in_sats += utxo.1.amount.as_sat();
+            selected_utxos.push(utxo);
+
+            if in_sats >= min_amt_sat {
+                break;
+            }
+        }
+
+        if in_sats < min_amt_sat {
+            return Err(PsbtCreationError::InsufficientFunds);
+        }
+
+        if in_sats - min_amt_sat > DUST_LIMIT {
+            outputs.push(TxOut {
+                value: in_sats - min_amt_sat,
+                script_pubkey: self.new_change_addr().script_pubkey(),
+            });
+        }
+
+        let unsinged_tx = Transaction {
+            version: 1,
+            lock_time: 0,
+            input: selected_utxos
+                .iter()
+                .map(|(deriv, utxo)| TxIn {
+                    previous_output: OutPoint::new(utxo.txid, utxo.vout),
+                    script_sig: Script::new(),
+                    sequence: 0,
+                    witness: vec![],
+                })
+                .collect(),
+            output: outputs,
+        };
+
+        let mut psbt = PartiallySignedTransaction::from_unsigned_tx(unsinged_tx).unwrap();
+
+        for ((deriv, utxo), input) in zip(selected_utxos, psbt.inputs.iter_mut()) {
+            *input = Self::descriptor_utxo_to_input(
+                utxo,
+                self.descriptor.derive(deriv.as_ref()),
+                deriv.clone(),
+            );
+        }
+
+        Ok(psbt)
+    }
+}
+
+pub fn finalize(mut psbt: PartiallySignedTransaction) -> PartiallySignedTransaction {
+    for input in psbt.inputs.iter_mut() {
+        if let Some(utxo) = &input.witness_utxo {
+            assert!(utxo.script_pubkey.is_v0_p2wpkh());
+            assert_eq!(input.partial_sigs.len(), 1);
+            let (key, sig) = input.partial_sigs.iter().next().unwrap();
+
+            let witness = vec![sig.clone(), key.to_bytes()];
+            input.final_script_witness = Some(witness);
+        } else {
+            unimplemented!()
+        }
+    }
+    psbt
+}
+
+fn print_psbt(psbt: &PartiallySignedTransaction) {
+    let mut psbt_bytes = vec![];
+    psbt.consensus_encode(&mut psbt_bytes).unwrap();
+    println!("psbt: {}", base64::encode(psbt_bytes));
+}
+
+#[derive(StructOpt)]
+enum Command {
+    List,
+    NewAddr,
+    Send { recipient: Address, amount: Amount },
+}
+
+fn main() {
+    let cmd: Command = Command::from_args();
+
+    let ctx = secp256k1::Secp256k1::new();
+
+    let mut xpriv = ExtendedPrivKey::from_str("tprv8ZgxMBicQKsPe2z4yh5peQ8VUQpvcmNH3zQ1gP7h3X41KWP76opG9BjVywxV2WfhxoVbEFfVjD6jjmR7ZM9NUBqQZmrhY1EuvbVQqY4VYKV").unwrap();
+    let xpub = ExtendedPubKey::from_private(&ctx, &xpriv);
+
+    let rpc = bitcoincore_rpc::Client::new(
+        "http://127.0.0.1:18443".into(),
+        Auth::UserPass("bitcoin".into(), "bitcoin".into()),
+    )
+    .unwrap();
+
+    let mut wallet = NaiveWallet::new(
+        format!("wpkh({}/*)", &xpub).parse().unwrap(),
+        Network::Regtest,
+        rpc,
+        10,
+    )
+    .unwrap();
+
+    match cmd {
+        Command::List => {
+            println!("descriptor: {}", &wallet.descriptor);
+            println!("funds: {} BTC", wallet.total_funds().as_btc());
+        }
+        Command::NewAddr => {
+            println!("{}", wallet.new_addr());
+        }
+        Command::Send { recipient, amount } => {
+            let tx = wallet
+                .create_transaction(&[TxOut {
+                    value: amount.as_sat(),
+                    script_pubkey: recipient.script_pubkey(),
+                }])
+                .unwrap();
+
+            print!("created ");
+            print_psbt(&tx);
+
+            let signed_tx = xpriv.sign_psbt(tx).unwrap();
+            print!("signed ");
+            print_psbt(&signed_tx);
+
+            let finalized_psbt = finalize(signed_tx);
+            print!("finalized ");
+            print_psbt(&finalized_psbt);
+
+            let mut tx_bytes = vec![];
+            let tx = finalized_psbt.extract_tx();
+            tx.consensus_encode(&mut tx_bytes).unwrap();
+            println!("tx: {}", hex::encode(&tx_bytes));
+
+            println!("{:?}", wallet.rpc.send_raw_transaction(&tx_bytes));
+        }
+    }
+}
---
+++ src/responsibilities.rs
@@ -0,0 +1,166 @@
+use bitcoin::util::bip32::{DerivationPath, ExtendedPrivKey, ExtendedPubKey, Fingerprint};
+use bitcoin::util::psbt::PartiallySignedTransaction;
+use bitcoin::{Address, Amount, Script, TxOut};
+use secp256k1::{Message, Signature};
+use std::fmt::Debug;
+
+/// This trait corresponds to the Creator and Updater responsibility described in
+/// [BIP-174](https://github.com/bitcoin/bips/blob/master/bip-0174.mediawiki#creator). As noted
+/// there the both functionalities are most often combined as the creator most likely has more meta
+/// information about the transaction they are creating and is thus able to put it into the PSBT
+/// (which otherwise would be a separate update step).
+pub trait PsbtWallet {
+    /// Wallet backend specific errors like database connection errors that aren't captured in
+    /// `PsbtCreationError`.
+    type Error: Debug;
+
+    /// Create a transaction that pays a set of outputs.
+    fn create_transaction(
+        &mut self,
+        outputs: &[TxOut],
+    ) -> Result<PartiallySignedTransaction, PsbtCreationError<Self::Error>>;
+
+    /// Creates a transaction that pays a certain `amount` to a `script` and keeps the change
+    fn pay_to_script(
+        &mut self,
+        script_pk: Script,
+        amount: Amount,
+    ) -> Result<PartiallySignedTransaction, PsbtCreationError<Self::Error>> {
+        self.create_transaction(&[TxOut {
+            value: amount.as_sat(),
+            script_pubkey: script_pk,
+        }])
+    }
+
+    /// Creates a transaction that pays a certain `amount` to an `address` and keeps the change
+    fn pay_to_address(
+        &mut self,
+        address: Address,
+        amount: Amount,
+    ) -> Result<PartiallySignedTransaction, PsbtCreationError<Self::Error>> {
+        self.pay_to_script(address.script_pubkey(), amount)
+    }
+}
+
+/// Common errors when creating a transaction
+#[derive(Debug)]
+pub enum PsbtCreationError<E: Debug> {
+    /// The wallet doens't control a sufficient amount of Bitcoins to fund the transaction
+    InsufficientFunds,
+    /// One of the outputs has a value below the dust limit
+    OutputBelowDustLimit,
+    /// Too many outputs were supplied
+    TooManyOutputs,
+    /// Wallet backend error
+    WalletError(E),
+}
+
+/// A signer capable of signing PSBTs. This can either be a software signer (see the implementation
+/// for `ExtendedSecretKey`) or a hardware device.
+pub trait SignPsbt {
+    /// Signing backend error type
+    type Error: Debug;
+
+    /// PSBT validation function that can be used by custom signers too
+    fn validate(&self, psbt: &PartiallySignedTransaction) -> Result<(), PsbtValidationError> {
+        // default validator impl
+        Ok(())
+    }
+
+    /// Signs all inputs for which it controls the keys and adds the signatures to the PSBT
+    fn sign_psbt(
+        &mut self,
+        psbt: PartiallySignedTransaction,
+    ) -> Result<PartiallySignedTransaction, PsbtSignError<Self::Error>>;
+}
+
+/// Common errors in the signing stage
+#[derive(Debug)]
+pub enum PsbtSignError<E: Debug> {
+    /// The PSBT is not valid for signig according to BIP-174
+    ValidationError(PsbtValidationError),
+    /// Error of the signing backend
+    BackendError(E),
+}
+
+/// Errors that can happen when validating a PSBT before signing
+#[derive(Debug)]
+pub enum PsbtValidationError {}
+
+impl<T, E> SignPsbt for T
+where
+    T: Fn(Message, Fingerprint, DerivationPath) -> Result<Option<Signature>, E>,
+    E: Debug,
+{
+    type Error = E;
+
+    fn sign_psbt(
+        &mut self,
+        mut psbt: PartiallySignedTransaction,
+    ) -> Result<PartiallySignedTransaction, PsbtSignError<Self::Error>> {
+        self.validate(&psbt)
+            .map_err(PsbtSignError::ValidationError)?;
+
+        let mut hasher = bitcoin::util::bip143::SigHashCache::new(&psbt.global.unsigned_tx);
+
+        for (input_idx, input) in psbt.inputs.iter_mut().enumerate() {
+            for (pk, (fp, path)) in input.hd_keypaths.iter() {
+                let sighash_type = input.sighash_type.map(|sht| sht.as_u32()).unwrap_or(1);
+
+                let script = &input.witness_utxo.as_ref().unwrap().script_pubkey;
+
+                let mut script_code = vec![0x76u8, 0xa9, 0x14];
+                script_code.extend_from_slice(&script[2..]);
+                script_code.extend_from_slice(&[0x88, 0xac]);
+
+                let sig_hash = hasher.signature_hash(
+                    input_idx,
+                    &script_code.into(),
+                    input.witness_utxo.as_ref().unwrap().value,
+                    input.sighash_type.unwrap(),
+                );
+
+                // TODO: impl 32bytes hash for hash newtypes
+                let msg = Message::from_slice(&sig_hash[..]).unwrap();
+                let sig: Signature = match self(msg, (*fp).clone(), path.clone())
+                    .map_err(|e| PsbtSignError::BackendError(e))?
+                {
+                    Some(sig) => sig,
+                    None => continue, // ignore keys we can't sign for
+                };
+
+                let mut sig_bytes = sig.serialize_der().to_vec();
+                sig_bytes.push(sighash_type as u8); // TODO: is this sane?
+                input.partial_sigs.insert(*pk, sig_bytes);
+            }
+        }
+
+        Ok(psbt)
+    }
+}
+
+impl SignPsbt for ExtendedPrivKey {
+    type Error = secp256k1::Error;
+
+    fn sign_psbt(
+        &mut self,
+        psbt: PartiallySignedTransaction,
+    ) -> Result<PartiallySignedTransaction, PsbtSignError<Self::Error>> {
+        let mut sign_closure = |msg, fp, path| -> Result<Option<Signature>, Self::Error> {
+            let ctx = secp256k1::Secp256k1::new();
+            // TODO: check path match … determine remaining path
+            /*if self.fingerprint(&ctx) != fp {
+                return Ok(None);
+            }*/
+
+            let key = match self.derive_priv(&ctx, &path) {
+                Ok(key) => key,
+                Err(bitcoin::util::bip32::Error::Ecdsa(e)) => return Err(e),
+                _ => unreachable!(),
+            };
+
+            Ok(Some(ctx.sign(&msg, &key.private_key.key)))
+        };
+        sign_closure.sign_psbt(psbt)
+    }
+}
